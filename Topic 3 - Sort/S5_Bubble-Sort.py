3bd203897aef38d5a447f90fb235b9f9fef83f1a
QuietLamb <chhunchannygod@gmail.com>
2025-06-13 09:31:57 +0700

    T3 - S5

---
+++ Topic 3 - Sort/S5_Bubble-Sort.py
@@ -0,0 +1,13 @@
+def sort(n, s):
+    cnt = 0
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if s[j] > s[j + 1]:
+                s[j], s[j+1] = s[j+1], s[j] #swap
+                cnt += 1
+        print(s, cnt)
+    return cnt
+
+N = int(input())
+S = list(map(int, input().split()))
+print(sort(N, S))
