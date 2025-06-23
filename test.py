780825f76ead166efc7dd49b88e8c384c231e098
QuietLamb <chhunchannygod@gmail.com>
2025-06-23 11:08:01 +0700

    test1

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-def prime(N, M):
-    for i in range(N+1, M):
-        n = 1
-        for j in range(2, i):
-            if i % j == 0:
-                n = 0
-        if n == 1:
-            print(i)
-prime(12, 24)
-
-
-
+print('Hello')
+print('World')
