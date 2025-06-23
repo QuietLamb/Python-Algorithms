17f124bbc6fa818b94afd08c516be77ab4d751c8
QuietLamb <chhunchannygod@gmail.com>
2025-06-23 11:44:57 +0700

    T3 - S6

---
+++ Topic 3 - Sort/S6_Selection-Sort.py
@@ -0,0 +1,14 @@
+def selection(n, s):
+    cnt = 0
+    for i in range(n - 1):
+        minv, minj = s[i], i
+        for j in range(i+1, n):
+            if s[j] < minv:
+                minv, minj = s[j], j
+        if i != minj:
+            s[i], s[minj] = s[minj], s[j]
+            cnt += 1
+    return cnt     
+N = int(input())
+S = list(map(int, input().split()))  
+print(selection(N, S)) 
