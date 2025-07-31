db3486f15d823d14313099199ece1945142f2bf5
QuietLamb <chhunchannygod@gmail.com>
2025-07-31 08:51:18 +0700

    T4 - S3

--- Topic 4 - Sequential-Search/S2_Palindrome.py
+++ Topic 4 - Sequential-Search/S2_Palindrome.py
@@ -6,9 +6,9 @@ def palindrome(n):
     return int(s) == int(r)  # Check if original and reversed are equal
 def solve(n, m):
     cnt = 0
-    for i in range(n, m+1):
-        if palindrome(i):
-            cnt += 1
+    for i in range(n, m+1):      # Loop from n to m (inclusive)
+        if palindrome(i):        # If number is a palindrome
+            cnt += 1             # Count it
     return cnt
 N, M = map(int, input().split())
 print(solve(N, M))
